58f189bae704dcbe7660fa5cf2cd5efb8ffa792e
hr097 <harshilramani9777@gmail.com>
2022-08-28 00:38:58 +0530

    db file was corrected

--- DATABASE/MYSQL FILE/james.sql
+++ DATABASE/MYSQL FILE/james.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2022 at 12:24 AM
+-- Generation Time: Aug 28, 2022 at 12:38 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
