bfaeb0f8c2a254742ec51cea743ccec4d7964248
hr097 <harshilramani9777@gmail.com>
2022-10-16 19:39:53 +0530

    feeddback form added

---
+++ ams-dev/js/student/feedback.js
@@ -0,0 +1,77 @@
+/* START::MODAL */
+
+var modal = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0]; //close modal
+
+span.onclick = function () {
+  //close modal
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  //close modal anywhere click
+  if (event.target == modal) {
+    modal.style.display = "none";
+    window.location.href = "./feedback.php";
+  }
+};
+
+document.getElementById("yes-button").onclick = function () {
+  // yes-> redirect
+  window.location.href = "./feedback.php";
+};
+
+/* END::MODAL */
+
+$(document).ready(function () {
+    
+          var csrfToken = $("#csrfToken");
+          var fbTxt = $("#feedbacktxt");
+
+          $("#submitfeedback").click(function () {
+
+              $.post(
+                "./submitfeedback.php",
+                {
+                  _fb: fbTxt.val(),
+                  _rt: $(".forms-sample input[name='rating']:checked").val(),
+                  _ct: csrfToken.val()
+                },
+                function (data, status) {
+                
+                  if(status == "success")
+                  {
+                    response = parseInt(data);
+  
+                    $("#modal").css("display","block");
+                    $("#yes-button").text("Okay");
+  
+                    if(response === 0)
+                    {
+                      $("#modalmsg").text("Sorry,Feedback can't be submitted!");
+                    }
+                    else if (response === 1)
+                    {
+                      $("#modalmsg").text("Your feedback has been submitted successfully.");
+                    }
+                    else
+                    {
+                        $("#modalmsg").text(
+                            "Try again later! Some unknown error occured."
+                        );
+                    }
+
+                  }
+                  else
+                  { 
+                    $("#modal").css("display", "block");
+                    $("#modalmsg").text(
+                      "Try again later! Some unknown error occured."
+                    );
+                    $("#yes-button").text("Okay");
+                  }
+        });
+
+    });
+});
+
+     
--- ams-dev/student/feedback.php
+++ ams-dev/student/feedback.php
@@ -8,18 +8,6 @@ if (!($JAMES->checkSession()&&$_SESSION["_userType"]==="1")) {
     $JAMES->ams_redirect("../login.php");
 }
 
-// $u = $_SESSION["_userId"];
-
-// //@query
-// $sql = "select *,DATE_FORMAT(dob,'%d-%m-%Y')AS dob from vw_students where email='$u';";
-// $result = mysqli_query($JAMES->connection(), $sql);
-
-// if (mysqli_num_rows($result)===1) {
-//     $user = mysqli_fetch_assoc($result);
-// } else {
-//     $JAMES->ams_redirect("../login.php");
-// }
-
 ?>
 
 <!DOCTYPE html>
@@ -29,11 +17,14 @@ if (!($JAMES->checkSession()&&$_SESSION["_userType"]==="1")) {
     <!-- including header -->
     <?php
     require_once('./common/header.php');
-?>
+    ?>
 
     <!-- css  -->
     <link rel="stylesheet" href="../css/student.css">
+    <link rel="stylesheet" href="../css/modal.css">
 
+    <!-- js  -->
+    <script src="../js/student/feedback.js" type="text/javascript" defer=true></script>
 
     <!-- page information-->
     <title>AMS | Feedback</title>
@@ -52,30 +43,32 @@ if (!($JAMES->checkSession()&&$_SESSION["_userType"]==="1")) {
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">AMS | Feedback</h4>
-                            <form class="forms-sample">
+                            <form class="forms-sample" method="post" action="">
 
                                 <div class="feedback-star-div">
                                     <ul class="rate-area">
-                                           <li><input type="radio"  id="5-star" name="rating" value="5"/><label for="5-star"
-                                            title="Outstanding" class="star_css">5 stars</label></li>
-                                            <li><input type="radio" id="4-star" name="rating" value="4" /><label for="4-star"
-                                            title="Excellent" class="star_css">4 stars</label></li>
-                                            <li><input type="radio" id="3-star" name="rating" value="3" checked/><label for="3-star"
-                                            title="Satisfactory" class="star_css">3 stars</label></li>
-                                            <li><input type="radio" id="2-star" name="rating" value="2" /><label for="2-star"
-                                            title="Good" class="star_css">2 stars</label></li>
-                                            <li><input type="radio" id="1-star" name="rating" value="1" /><label for="1-star"
-                                            title="Worst" class="star_css">1 star</label></li>
+                                        <input type="radio"  id="5-star" name="rating" value="5"/><label for="5-star"
+                                            title="Outstanding" class="star_css">5 stars</label>
+                                        <input type="radio" id="4-star" name="rating" value="4" checked/><label for="4-star"
+                                            title="Excellent" class="star_css">4 stars</label>
+                                        <input type="radio" id="3-star" name="rating" value="3" /><label for="3-star"
+                                            title="Satisfactory" class="star_css">3 stars</label>
+                                        <input type="radio" id="2-star" name="rating" value="2" /><label for="2-star"
+                                            title="Good" class="star_css">2 stars</label>
+                                        <input type="radio" id="1-star" name="rating" value="1" /><label for="1-star"
+                                            title="Worst" class="star_css">1 star</label>
                                     </ul>
-
+ 
                                 </div>
+                                <input type="hidden" id="csrfToken" name="_csrfToken" value="<?php echo $JAMES->generateCsrfToken();?>"> 
+                                
                                 <!-- Subject -->
                                 <div class="form-group">
-                                    <input type="textarea" class="form-control feedback-textarea"
+                                    <input type="textarea" id="feedbacktxt" name="feedback" class="form-control feedback-textarea"
                                         placeholder="Give your valuable feedback to us">
                                 </div>
 
-                                <button type="submit" class="btn btn-primary mr-2 mt-3">Submit</button>
+                                <button type="button" id="submitfeedback" class="btn btn-primary mr-2 mt-3">Submit</button>
                                 <button class="btn btn-light mt-3">Clear</button>
                             </form>
                         </div>
@@ -85,6 +78,17 @@ if (!($JAMES->checkSession()&&$_SESSION["_userType"]==="1")) {
             </div>
         </div>
 
+    </div>
+
+    <!-- modal -->
+    <div id="modal" class="modal">
+    <!-- modal content -->
+    <div class="modal-content" style="width:360px;">
+            <span class="close">&times;</span>
+            <p class="msg unselectable" id="modalmsg"></p>
+            <div class="row" style="margin:auto;margin-bottom:30px;">
+            <button id="yes-button" class="modal-btn"></button>
+    </div>
     </div>
 
     <!-------------------------------------------------------Main Content End------------------------------------------------------->
