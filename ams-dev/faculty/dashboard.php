814a83ab1d1f83dc4893ead27b1a11f237a63142
Archit Ghevariya <ghevariyaarchit@gmail.com>
2022-10-16 22:24:37 +0530

    faculty dashboard updated

--- ams-dev/faculty/dashboard.php
+++ ams-dev/faculty/dashboard.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -18,85 +18,134 @@
 </head>
 
 <body>
-       <!-------------------------------------------------------Main Content------------------------------------------------------->
-       <div class="main-panel">
+    <!-------------------------------------------------------Main Content------------------------------------------------------->
+
+    <div class="main-panel">
         <div class="content-wrapper">
-        <div class="row">
-             <div class="col-12 col-xl-8 mb-4 mb-xl-50">
-                 <h3 class="font-weight-bold">Welcome Sir,</h3>
-                 <h6 class="font-weight-normal mb-10">Good Morning, </h6>
-             </div>
-            
-          <!-------------------------------------------------------States------------------------------------------------------->
-          <div class="col-md-12 grid-margin transparent">
             <div class="row">
-              <div class="col-md-3 mb-2 stretch-card transparent lblmargin handpointer" onclick="subopen()">
-                <div class="card card-dark-blue ">
-                  <div class="card-body">
-                    <p class="mb-2 subfont">IT-2022</p>
-                    <p class="mb-4 subfont">Web Development</p>
-                    <p>Semester :  4th</p>
-                    <p>Division :  A</p>
-                  </div>
+                <div class="col-12 col-xl-8 mb-4 mb-xl-50">
+                    <h3 class="font-weight-bold">Welcome Sir,</h3>
+                    <h6 class="font-weight-normal mb-10">Good Morning, </h6>
                 </div>
-              </div>
-
-              <div class="col-md-3 mb-2 stretch-card transparent handpointer" onclick="subopen()">
-                <div class="card card-tale">
-                  <div class="card-body">
-                  <p class="mb-2 subfont">IT-2022</p>
-                    <p class="mb-4 subfont">RDBMS</p>
-                    <p>Semester :  4th</p>
-                    <p>Division :  A</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3 mb-2 stretch-card transparent handpointer" onclick="subopen()">
-                <div class="card card-light-danger">
-                  <div class="card-body">
-                  <p class="mb-2 subfont">IT-2022</p>
-                    <p class="mb-4 subfont">IOT</p>
-                    <p>Semester :  4th</p>
-                    <p>Division :  A</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3 mb-2 stretch-card transparent handpointer" onclick="subopen()">
-                <div class="card card-dark-blue bg-warning">
-                  <div class="card-body">
-                  <p class="mb-2 subfont">IT-2022</p>
-                    <p class="mb-4 subfont">Enviromental Science</p>
-                    <p>Semester :  4th</p>
-                    <p>Division :  A</p>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-          <!----------------------------------------------Report Generate End---------------------------------------------->
-          <!--Subject Page Open Start-->
-          <script>
-            function subopen() {
-              window.location = "./stud.php";
-            }
-          </script>
-          <!--Subject Page Open End-->
-
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
-<!-- including footer -->
-<?php
+
+            <!-- Classroom sorting --> 
+                <div class="row">
+                    <div class="form-group col-md-3">
+                            <label>Year</label>
+                            <select class="form-control">
+                                <option>2022</option>
+                                <option>2021</option>
+                            </select>
+                        </div>
+
+                    <div class="form-group col-md-3 ">
+                        <label>Course</label>
+                        <select class="form-control">
+                            <option>IT</option>
+                            <option>ICT</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-3 ">
+                        <label>Semester</label>
+                        <select class="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-3 ">
+                        <label>Division</label>
+                        <select class="form-control">
+                            <option>A</option>
+                            <option>B</option>
+                            <option>C</option>
+                            <option>D</option>
+                        </select>
+                    </div>
+                  
+            <!-------------------------------------------------------States------------------------------------------------------->
+            
+            <div class="col-md-12 grid-margin transparent">
+                <div class="row">
+
+                    <div class="col-md-3 mb-2 stretch-card transparent lblmargin handpointer" onclick="subopen()">
+                        <div class="card card-dark-blue ">
+                            <div class="card-body">
+                                <p class="mb-2 subfont">IT-2022</p>
+                                <p class="mb-4 subfont">Web Development</p>
+                                <p>Semester : 4th</p>
+                                <p>Division : A</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 mb-2 stretch-card transparent handpointer" onclick="subopen()">
+                        <div class="card card-tale">
+                            <div class="card-body">
+                                <p class="mb-2 subfont">IT-2022</p>
+                                <p class="mb-4 subfont">RDBMS</p>
+                                <p>Semester : 4th</p>
+                                <p>Division : A</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 mb-2 stretch-card transparent handpointer" onclick="subopen()">
+                        <div class="card card-light-danger">
+                            <div class="card-body">
+                                <p class="mb-2 subfont">IT-2022</p>
+                                <p class="mb-4 subfont">IOT</p>
+                                <p>Semester : 4th</p>
+                                <p>Division : A</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 mb-2 stretch-card transparent handpointer" onclick="subopen()">
+                        <div class="card card-dark-blue bg-warning">
+                            <div class="card-body">
+                                <p class="mb-2 subfont">IT-2022</p>
+                                <p class="mb-4 subfont">Enviromental Science</p>
+                                <p>Semester : 4th</p>
+                                <p>Division : A</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+
+
+    <!----------------------------------------------Report Generate End---------------------------------------------->
+    <!--Subject Page Open Start-->
+    <script>
+    function subopen() {
+        window.location = "./stud.php";
+    }
+    </script>
+    <!--Subject Page Open End-->
+
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <!-- including footer -->
+    <?php
     include './common/footer.php'
     ?>
 </body>
 
-</html>
+</html> -->
--- ams-dev/student/feedback.php
+++ ams-dev/student/feedback.php
@@ -57,18 +57,18 @@ if (mysqli_num_rows($result)===1) {
                                 <div class="feedback-star-div">
                                     <ul class="rate-area">
                                         <input type="radio" id="5-star" name="rating" value="5" /><label for="5-star"
-                                            title="Amazing">5 stars</label>
+                                            title="Amazing" class="star_css">5 stars</label>
                                         <input type="radio" id="4-star" name="rating" value="4" /><label for="4-star"
-                                            title="Good">4 stars</label>
+                                            title="Good" class="star_css">4 stars</label>
                                         <input type="radio" id="3-star" name="rating" value="3" /><label for="3-star"
-                                            title="Average">3 stars</label>
+                                            title="Average" class="star_css">3 stars</label>
                                         <input type="radio" id="2-star" name="rating" value="2" /><label for="2-star"
-                                            title="Not Good">2 stars</label>
+                                            title="Not Good" class="star_css">2 stars</label>
                                         <input type="radio" id="1-star" name="rating" value="1" /><label for="1-star"
-                                            title="Bad">1 star</label>
+                                            title="Bad" class="star_css">1 star</label>
                                     </ul>
 
-                                </div>
+                                </div>  
                                 <!-- Subject -->
                                 <div class="form-group">
                                     <input type="textarea" class="form-control feedback-textarea"
