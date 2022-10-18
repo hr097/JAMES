169d42c8349bfbd7063cc5a66ce05febd8c0890a
hr097 <harshilramani9777@gmail.com>
2022-10-18 22:15:13 +0530

    harshil

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,32 +1,59 @@
+<?php
+
+ require_once("../ams.php");
+ $JAMES = new AMS("Admin");
+ $JAMES->init_user_session();
+
+
+ if(!($JAMES->checkSession()&&$_SESSION["_userType"]==="3"))
+ {
+  $JAMES->ams_redirect("../login.php");
+ }
+?>
 
 <!DOCTYPE html>
-
 <html lang="en">
 
     <head>
 
-        <title>AMS | Admin Dashboard</title>
+        <!-- including header -->
+        <?php
+        include './common/header.php'
+        ?>
 
-        <link rel="icon" href="" type="image/icon type">
+        <!-- css  -->
+        <link rel="stylesheet" href="../css/admin.css">
+        
+        <!-- js  -->
+        <script src="../js/admin/dashboard.js" type="text/javascript" defer=true></script>
 
-        <style type="text/css">
-
-        </style>
-
+        <!-- page information-->
+        <title>AMS | Admin dashboard</title>
     </head>
 
     <body>
 
-    <br><b>This is Admin dashboard</b>
+                <!-------------------------------------------------------Main Content------------------------------------------------------->
+                <div class="main-panel">
+                    <div class="content-wrapper">
+                        <div class="row">
+                            <div class="col-12 col-xl-8 mb-4 mb-xl-50">
+                                <h3 class="font-weight-bold">Welcome <?php echo $user['fname']; ?>,</h3>
+                                <h6 id="daymode" class="font-weight-normal mb-10"></h6>
+                            </div>
 
-    <br><br><br>
-    
-    <a href='../php/logout.php'><button type="button">Logout</button></a>
+
+                        </div>
+                    </div>
+                </div>
+
+        <!-- including footer -->
+        <?php
+        include './common/footer.php'
+        ?>
 
     </body>
 
-    <script type="text/javascript"></script>
-
-    <noscript>Sorry, Your browser does not support JavaScript !!!</noscript>
 
 </html>
+
--- ams.php
+++ ams.php
@@ -257,7 +257,7 @@ class AMS
         //Enable SMTP debugging.
         // $mail->SMTPDebug = 3;         for debugging                      
         //Set PHPMailer to use SMTP.
-      //  $mail->isSMTP();           //@@@@@@@@ changed as vnsgu server can't send SMTP email it will send it from root domain    
+        //  $mail->isSMTP();           //@@@@@@@@ changed as vnsgu server can't send SMTP email it will send it from root domain    
         //Set SMTP host name                          
         $mail->Host = "smtp.gmail.com";
         //Set this to true if SMTP host requires authentication to send email
