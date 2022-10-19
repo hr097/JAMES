2534df0bae868fcd689655fa4ee511bd0a8635ab
Archit Ghevariya <ghevariyaarchit@gmail.com>
2022-10-19 11:47:03 +0530

    ams.php file updated

--- ams.php
+++ ams.php
@@ -304,7 +304,7 @@ class AMS
         }
         else// if($userType=="User" || $userType=="") // BY DEFAULT DB WILL  BE CONNECTED AS USER IF NOTHING IS PASSED
         {
-            $userType=3;
+            $userType=2;
         }
         
         //$userType=3; //!localhost development enabled
