e2750811a26d632f1c156689357bf13ef37139e6
hr097 <harshilramani9777@gmail.com>
2022-10-19 14:03:29 +0530

    hr: admin added

--- admin/common/header.php
+++ admin/common/header.php
@@ -116,8 +116,8 @@
                         </a>
                         <div class="collapse" id="ui-basic">
                             <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="./facultyregistartion.php">Faculty</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="./studentregistartion.php">Student</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="./facultyregistration.php">Faculty</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="./studentregistration.php">Student</a></li>
                             </ul>
                         </div>
                     </li>
