98c9ef91cd0b80b11e46f008d778280af3b97d1f
hr097 <harshilramani9777@gmail.com>
2022-10-26 17:07:22 +0530

    hr: faculty profile

--- faculty/profile.php
+++ faculty/profile.php
@@ -1,3 +1,33 @@
+<?php
+
+require_once("../ams.php");
+$JAMES = new AMS("User");
+$JAMES->init_user_session();
+
+if(!($JAMES->checkSession()&&$_SESSION["_userType"]==="2"))
+{
+ $JAMES->ams_redirect("../login.php");
+}
+
+$u = $_SESSION["_userId"];
+
+//@query
+$sql = "select *,DATE_FORMAT(dob,'%d-%m-%Y')AS dob from vw_faculties where email='$u';"; 
+$result = mysqli_query($JAMES->connection(),$sql);
+
+if(mysqli_num_rows($result)===1)
+{
+    $user = mysqli_fetch_assoc($result);
+}
+else
+{
+    $JAMES->ams_redirect("../login.php");
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -35,16 +65,29 @@
                       <!-- Front-side -->
                         <div class="flip-card">
                             <div class="card__face card__face--front" style="border-radius: 10px;">
-                                <img src="../assets/profiles/faculty-profile.jpg" class="my-4" alt="Faculty profile"
-                                    style="width:130px;height:130px; border-radius: 49%;">
-                                <h3 style="color: white; margin-top: -15px;">Archit Ghevariya</h3>
+
+                                <!-- <img src="../assets/profiles/faculty-profile.jpg" class="my-4" alt="Faculty profile" style="width:130px;height:130px; border-radius: 49%;"> -->
+
+                                <?php
+                                    if($_SESSION['_gender']=="Male")
+                                    {
+                                        echo "<img src='../assets/profiles/faculty-profile-male.png' class='my-4' alt='Faculty profile' style='width:130px;height:130px; border-radius: 49%;'>";
+                                    }
+                                    else
+                                    {
+                                        echo "<img src='../assets/profiles/faculty-profile-female.jpg' class='my-4' alt='Faculty profile' style='width:130px;height:130px; border-radius: 49%;'>";
+                                    }   
+                                ?>
+
+
+                                <h3 style="color: white; margin-top: -15px;"><?php echo $user['name']; ?></h3>
                             </div>
 
                             <!-- back side -->
                             <div  class="card__face card__face--back py-4 pl-4" style="font-weight:500;font-size: 15px;" align="left">
                               <p class="mt-3">
-                              <span  class="card_back_title mr-5"> Faculty id :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
-                              <span class="card_back_data" style="font-weight:normal;">FID123</span>
+                              <span  class="card_back_title mr-5"> FID :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
+                              <span class="card_back_data" style="font-weight:normal;"><?php echo $user['fid']; ?></span>
                               </p>
 
                               <p>
@@ -54,18 +97,18 @@
 
                               <p>
                               <span  class="card_back_title mr-5"> Designation :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
-                              <span lass="card_back_data" style="font-weight:normal;" >Assistant</span>
+                              <span lass="card_back_data" style="font-weight:normal;" ><?php echo $user['designation']; ?></span>
                               </p>
 
                               <p>
                               <span  class="card_back_title mr-5"> Joining year :&nbsp&nbsp&nbsp&nbsp&nbsp</span>
-                              <span lass="card_back_data" style="font-weight:normal;" >18-5-2022</span>
+                              <span lass="card_back_data" style="font-weight:normal;" ><?php echo $user['joining_year']; ?></span>
                               </p>
 
-                              <p>
+                              <!-- <p>
                               <span  class="card_back_title mr-5"> Course name :&nbsp&nbsp&nbsp</span>
                               <span lass="card_back_data" style="font-weight:normal;" >IT</span>
-                              </p>
+                              </p> -->
                                   </div>
                               </div>
                               </div>
@@ -84,13 +127,13 @@
                         <div class="card-body">
 
                             <h6 class="info-title">Birth Date </h6>
-                            <h4 class="info-data">08-03-2003</h4>
+                            <h4 class="info-data"><?php echo $user['dob']; ?></h4>
 
                             <h6 class="info-title">Gender</h6>
-                            <h4 class="info-data">Male</h4>
+                            <h4 class="info-data"><?php echo $user['gender']; ?></h4>
 
                             <h6 class="info-title"> Joining Year</h6>
-                            <h4 class="info-data">2020</h4>
+                            <h4 class="info-data"><?php echo $user['joining_year']; ?></h4>
                         </div>
                     </div>
                 </div>
@@ -133,27 +176,13 @@
                     <div class="row" style="margin:auto;margin-bottom:30px;">
                     <button id="yes-button" class="modal-btn"></button>
             </div>
-        </div>
+    </div>
     <!-------------------------------------------------------Main Content End------------------------------------------------------->
-
-    <!-- <script>
-    // $(document).ready(() => {
-    //   $('#stud_card').click(function () {
-    //     $('#stud_card').flip({ trigger: "manual" });
-    //   });
-    // });
-    var card = document.querySelector('.flip-card');
-    card.addEventListener('click', function() {
-        card.classList.toggle('is-flipped');
-    });
-    </script> -->
 
     <!-- including footer -->
     <?php
     include './common/footer.php'
     ?>
-
-
 
 </body>
 
