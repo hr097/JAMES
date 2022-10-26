fcb8556a3ee562223a8dcc2378e94d3c5b52059f
hr097 <harshilramani9777@gmail.com>
2022-10-26 17:36:11 +0530

    hr: faculty profile

--- faculty/profile.php
+++ faculty/profile.php
@@ -100,10 +100,10 @@ else
                               <span lass="card_back_data" style="font-weight:normal;" ><?php echo $user['designation']; ?></span>
                               </p>
 
-                              <p>
+                              <!-- <p>
                               <span  class="card_back_title mr-5"> Joining year :&nbsp&nbsp&nbsp&nbsp&nbsp</span>
                               <span lass="card_back_data" style="font-weight:normal;" ><?php echo $user['joining_year']; ?></span>
-                              </p>
+                              </p> -->
 
                               <!-- <p>
                               <span  class="card_back_title mr-5"> Course name :&nbsp&nbsp&nbsp</span>
