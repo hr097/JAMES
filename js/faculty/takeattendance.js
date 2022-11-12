1378e0d7dfc41327737986c107dd80990af93483
hr097 <harshilramani9777@gmail.com>
2022-11-13 01:00:07 +0530

    take attendance ready

--- js/faculty/takeattendance.js
+++ js/faculty/takeattendance.js
@@ -178,16 +178,15 @@ $(document).ready(function(){
                 $("#modal").css("display","block");
               }
               else
-              {   console.log("1");
-              $("#order-listing").DataTable().destroy();
+              {   
                  let st_ls_len = student_list.response.length;
 
                  for(let i=0;i<st_ls_len;i++)
-                 {console.log("2");
+                 {
                    $(`#${student_list.response[i]}`).attr("checked",true);
                    $(`#${student_list.response[i]}`).siblings()[0].innerHTML = "1";
                  }
-                 console.log("3");
+                
               }
 
             }
