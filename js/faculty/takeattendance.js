c359f7e41204c7faa865142856543b7503e532c6
hr097 <harshilramani9777@gmail.com>
2022-11-13 00:58:18 +0530

    take attendance ready

--- js/faculty/takeattendance.js
+++ js/faculty/takeattendance.js
@@ -179,6 +179,7 @@ $(document).ready(function(){
               }
               else
               {   console.log("1");
+              $("#order-listing").DataTable().destroy();
                  let st_ls_len = student_list.response.length;
 
                  for(let i=0;i<st_ls_len;i++)
