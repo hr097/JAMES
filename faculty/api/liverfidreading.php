cd11865e5d8306c3810576239c2c2f3a4348f9c0
hr097 <harshilramani9777@gmail.com>
2022-11-13 20:01:02 +0530

    data table refresh

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -50,7 +50,7 @@ if(isset($_GET['cid'])&&isset($_SESSION['_userId']))
 {
         $cid = $JAMES->sanitizeInput($_GET['cid']);
         $message=getAmsApi($cid);
-        echo "data: ".$message."\n";
+        echo $message;
         flush();
 }
 else
