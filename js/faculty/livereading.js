ecd16d059537bdb2d6bc0c2d784134a15f81b755
hr097 <harshilramani9777@gmail.com>
2022-11-13 20:13:04 +0530

    SSE test

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
         if(typeof(EventSource) !== "undefined") {
             var source = new EventSource(`api/liverfidreading.php?cid=${classroomid}`);
 
-            // source.addEventListener("message", function(e) {
-            //     console.log(e.data)
-            //     $("#rfidcarddata").html(e.data);
-            //   })
+            source.addEventListener("message", function(e) {
+                console.log(e.data)
+                $("#rfidcarddata").html(e.data);
+              })
 
-            source.onmessage = function(event) {
-                document.getElementById("rfidcarddata").innerHTML = event.data;
-            };
+            // source.onmessage = function(event) {
+            //     document.getElementById("rfidcarddata").innerHTML = event.data;
+            // };
 
           } else {
             alert("Sorry, your browser does not support server-sent events...");
