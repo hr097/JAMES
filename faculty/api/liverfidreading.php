86893730c7657ce3cdddc300a88b9b20a36b0393
hr097 <harshilramani9777@gmail.com>
2022-11-13 20:06:36 +0530

    SSE test

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -12,7 +12,7 @@ $JAMES->init_user_session();
 
 function getAmsApi($classroomid) 
 {   
-    $sql= "select Students.spid,Students.name,Students.gender,Students.dob,Students.cur_semester FROM Students,Ams_api WHERE Ams_api.spid=Students.spid and Ams_api.reader_no=$classroomid;";
+    $sql= "select Students.spid,Students.name,Students.gender,Students.dob,Students.cur_semester FROM Students,Ams_api WHERE Ams_api.spid=Students.spid and Ams_api.reader_no=$classroomid and Ams_api.reading_no=max(Ams_api.reading_no);";
 
     $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
     
