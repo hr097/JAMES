867db5ad0bb9f971d5f587571af33d54034b8915
hr097 <harshilramani9777@gmail.com>
2022-11-13 20:03:17 +0530

    SSE test

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -50,7 +50,7 @@ if(isset($_GET['cid'])&&isset($_SESSION['_userId']))
 {
         $cid = $JAMES->sanitizeInput($_GET['cid']);
         $message=getAmsApi($cid);
-        echo $message;
+        echo "data: ".$message;
         flush();
 }
 else
