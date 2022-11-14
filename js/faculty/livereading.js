e09699b67ad0a7f25a767f9bba528208b1d9e1f6
hr097 <harshilramani9777@gmail.com>
2022-11-14 09:29:31 +0530

    card added

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -24,4 +24,10 @@ $(document).ready(function(){
 
     });
 
+  //Card flip
+  var card = document.querySelector(".flip-card");
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
+  });
+
 });
