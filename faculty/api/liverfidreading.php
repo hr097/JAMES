b710b6495cd6007179e48bbecc3f2f5f901ba7e7
hr097 <harshilramani9777@gmail.com>
2022-11-14 12:47:00 +0530

    shu

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -11,7 +11,7 @@ $JAMES->init_user_session();
 
 function getAmsApi($classroomid) 
 {   
-    $sql= "select Courses.course_name,Students.cur_semester,Students.cur_division,Students.spid,Students.name,Students.gender,DATE_FORMAT(Students.dob,'%d/%m/%Y') AS dob,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid order by Ams.api DESC;";
+    $sql= "select Courses.course_name,Students.cur_semester,Students.cur_division,Students.spid,Students.name,Students.gender,DATE_FORMAT(Students.dob,'%d/%m/%Y') AS dob,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid order by Ams_api.reading_no DESC;";
 
     $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
     
@@ -20,6 +20,7 @@ function getAmsApi($classroomid)
         $faculty = "";
         while($record = mysqli_fetch_assoc($result))
         {
+
         $faculty.=
         "
         <tr class='student'>
@@ -30,6 +31,7 @@ function getAmsApi($classroomid)
         <td>".$record['cur_semester']."</td>
         </tr>
         ";
+
         }
 
         return $faculty;
