5878fe00afca1e4bf68ee487ed6ce05efc7ca841
hr097 <harshilramani9777@gmail.com>
2022-11-14 16:31:04 +0530

    shu

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -1,17 +1,9 @@
 
-var data_bckp="";
-var req_num=1;
-
 function getLatestData()
 {
     let classroomid = $("#reader_selection").val();
     let csrfToken = $("#csrfToken").val();
     
-    if(req_num==1)
-    {
-        $("#rfidcarddata").html(null);
-    }
-
     $.post(
     "api/liverfidreading.php",
     {
@@ -21,27 +13,19 @@ function getLatestData()
     function (data, status) {
         if(status == "success")
         {         
-            if(req_num==1)  
-            {
-                $("#rfidcarddata").prepend(data);
-                data_bckp = data;
-                req_num=2;
-            }
-            else
-            {   
-                if(data==`<tr>
-                <td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td>
-                </tr>`)
-                {
-                    $("#rfidcarddata").html(null);
-                }
 
-                if(data_bckp!=data)
-                {      
-                    $("#rfidcarddata").prepend(data);
-                    data_bckp=data;
+                if(data==`<tr>
+                 <td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td>
+                 </tr>`)
+                {   
+                    $("#rfidcarddata").html(null);
+                    $("#rfidcarddata").html(data);
                 }
-            }
+                else
+                {
+                    $("#rfidcarddata").prepend(data);
+                }
+               
         }
     },"text"); // must write as text string will come
 }
@@ -49,7 +33,6 @@ function getLatestData()
 $(document).ready(function(){
 
     $("#reader_selection").on('change',function () {
-        
         setInterval(
         function ()
         {   
