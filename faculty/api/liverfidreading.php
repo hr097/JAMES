b971fd7087ddbdc1f5934e5caa99c8195ff946c3
hr097 <harshilramani9777@gmail.com>
2022-11-15 00:50:38 +0530

    oik

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -1,56 +1,42 @@
 <?php
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
-// header('Access-Control-Allow-Origin:ams.vnsguit.org'); 
-//header('Access-Control-Allow-Methods: POST');
-// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type, Access-Control-Allow-Methods,Authorization');
+header('Access-Control-Allow-Origin:ams.vnsguit.org'); 
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type, Access-Control-Allow-Methods,Authorization');
 
 
-// require_once("../../ams.php");
-// $JAMES = new AMS("User");
-// $JAMES->init_user_session();
+require_once("../../ams.php");
+$JAMES = new AMS("User");
+$JAMES->init_user_session();
 
-// function getAmsApi($classroomid) 
-// {   
-//     $sql= "select Students.cur_semester,Students.spid,Students.name,Students.gender,DATE_FORMAT(Ams_api.reading_date_time,'%d/%m/%Y %h:%i:%s') AS rdt,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no=(select MAX(Ams_api.reading_no) FROM Ams_api);";
+function getAmsApi($classroomid) 
+{   
+    $sql= "select Students.cur_semester,Students.spid,Students.name,Students.gender,DATE_FORMAT(Ams_api.reading_date_time,'%d/%m/%Y %h:%i:%s') AS rdt,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no=(select MAX(Ams_api.reading_no) FROM Ams_api);";
 
-//     $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
+    $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
     
-//     if(mysqli_num_rows($result)==1)
-//     {
-//         $faculty = "";
-//         $record = mysqli_fetch_assoc($result);
+    if(mysqli_num_rows($result)==1)
+    {
+        $record = mysqli_fetch_assoc($result);
+        $student.= $record['spid']."|".$record['name']."|".$record['gender']."|".$record['rdt']."|".$record['cur_semester'];
+        return $student;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
-//         $faculty.=
-//         "
-//         <tr class='student'>
-//         <td>".$record['spid']."</td>
-//         <td>".$record['name']."</td>
-//         <td>".$record['gender']."</td>
-//         <td>".$record['rdt']."</td>
-//         <td>".$record['cur_semester']."</td>
-//         </tr>
-//         ";
-
-//         return $faculty;
-//     }
-//     else
-//     {
-//         return 0;
-//     }
-// }
-
-//if(isset($_POST['_cid'])&&isset($_POST['_ct'])&&$_POST['_ct']==$_SESSION['_csrfToken']&&isset($_SESSION['_userId']))
-//{
-        //$cid = $JAMES->sanitizeInput($_POST['_cid']);
-       // $cid=2;
-       // echo "data: ".getAmsApi($cid);
-        echo "data: hello\n";
+if(isset($_GET['cid'])&&isset($_SESSION['_userId']))
+{
+        $cid = $JAMES->sanitizeInput($_GET['cid']);
+        echo "data: ".getAmsApi($cid)."\n";
         flush();
-//}
-//else
-//{    
-//    $JAMES->ams_redirect("../../login.php"); // when outside request comes redirect to login
-//}
+}
+else
+{    
+   $JAMES->ams_redirect("../../login.php"); // when outside request comes redirect to login
+}
 
 ?>
