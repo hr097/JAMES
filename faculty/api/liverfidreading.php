dc59ff5206d7991da7b7ea4df08e32abf410cbee
hr097 <harshilramani9777@gmail.com>
2022-11-14 09:51:24 +0530

    profile removed

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -66,7 +66,7 @@ function getAmsApi($classroomid)
 
                  <div class='card__face card__face--front' style='border-radius: 10px;'>
 
-                   <h4  class='profile_name' style='color:white;margin-top:22px;' >".$record['name']."</h4>
+                   <h4  class='profile_name' style='color:white;margin-top:52px;' >".$record['name']."</h4>
                  </div>
 
                   <div  class='card__face card__face--back py-4 pl-4' style='font-weight:500;font-size: 15px;' align='left'>
