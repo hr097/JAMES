768fbfb8e0a9e3bb44615b21d94ea75f21d8c2ef
hr097 <harshilramani9777@gmail.com>
2022-11-15 00:12:59 +0530

    oik

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -10,40 +10,40 @@ header('Cache-Control: no-cache');
 // $JAMES = new AMS("User");
 // $JAMES->init_user_session();
 
-function getAmsApi($classroomid) 
-{   
-    $sql= "select Students.cur_semester,Students.spid,Students.name,Students.gender,DATE_FORMAT(Ams_api.reading_date_time,'%d/%m/%Y %h:%i:%s') AS rdt,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no=(select MAX(Ams_api.reading_no) FROM Ams_api);";
+// function getAmsApi($classroomid) 
+// {   
+//     $sql= "select Students.cur_semester,Students.spid,Students.name,Students.gender,DATE_FORMAT(Ams_api.reading_date_time,'%d/%m/%Y %h:%i:%s') AS rdt,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no=(select MAX(Ams_api.reading_no) FROM Ams_api);";
 
-    $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
+//     $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
     
-    if(mysqli_num_rows($result)==1)
-    {
-        $faculty = "";
-        $record = mysqli_fetch_assoc($result);
+//     if(mysqli_num_rows($result)==1)
+//     {
+//         $faculty = "";
+//         $record = mysqli_fetch_assoc($result);
 
-        $faculty.=
-        "
-        <tr class='student'>
-        <td>".$record['spid']."</td>
-        <td>".$record['name']."</td>
-        <td>".$record['gender']."</td>
-        <td>".$record['rdt']."</td>
-        <td>".$record['cur_semester']."</td>
-        </tr>
-        ";
+//         $faculty.=
+//         "
+//         <tr class='student'>
+//         <td>".$record['spid']."</td>
+//         <td>".$record['name']."</td>
+//         <td>".$record['gender']."</td>
+//         <td>".$record['rdt']."</td>
+//         <td>".$record['cur_semester']."</td>
+//         </tr>
+//         ";
 
-        return $faculty;
-    }
-    else
-    {
-        return 0;
-    }
-}
+//         return $faculty;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
 
 //if(isset($_POST['_cid'])&&isset($_POST['_ct'])&&$_POST['_ct']==$_SESSION['_csrfToken']&&isset($_SESSION['_userId']))
 //{
         //$cid = $JAMES->sanitizeInput($_POST['_cid']);
-        $cid=2;
+       // $cid=2;
        // echo "data: ".getAmsApi($cid);
         echo "data: hello";
         flush();
