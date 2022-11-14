8a23a49714a0cf8a47d009e5ffa7e0a3223a4856
hr097 <harshilramani9777@gmail.com>
2022-11-15 00:29:07 +0530

    oik

--- faculty/api/amsapilength.php
+++ faculty/api/amsapilength.php
@@ -2,10 +2,43 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-$api="ok".$_GET['cid'];
-$data="Hello".$api."ok";
+require_once("../../ams.php");
+$JAMES = new AMS("User");
+$JAMES->init_user_session();
 
 
+function getAmsApi($classroomid) 
+{   
+    $sql= "select Students.cur_semester,Students.spid,Students.name,Students.gender,DATE_FORMAT(Ams_api.reading_date_time,'%d/%m/%Y %h:%i:%s') AS rdt,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no=(select MAX(Ams_api.reading_no) FROM Ams_api);";
+
+    $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
+    
+    if(mysqli_num_rows($result)==1)
+    {
+        $faculty = "";
+        $record = mysqli_fetch_assoc($result);
+
+        $faculty.=
+        "
+        <tr class='student'>
+        <td>".$record['spid']."</td>
+        <td>".$record['name']."</td>
+        <td>".$record['gender']."</td>
+        <td>".$record['rdt']."</td>
+        <td>".$record['cur_semester']."</td>
+        </tr>
+        ";
+
+        return $faculty;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+$_GET['cid'];
+$data=getAmsApi($_GET['cid']);
 echo "data: {$data}\n\n";
 flush();
 ?>
