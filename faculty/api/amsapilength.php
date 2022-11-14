6733c9d8a970d41605e4b3e6f10bb8bd8858048f
hr097 <harshilramani9777@gmail.com>
2022-11-15 00:23:21 +0530

    oik

--- faculty/api/amsapilength.php
+++ faculty/api/amsapilength.php
@@ -2,13 +2,7 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-$data="<tr class='student'>
-         <td>.record['spid'].</td>
-         <td>.record['name'].</td>
-        <td>.record['gender'].</td>
-        <td>.record['rdt'].</td>
-        <td>.record['cur_semester'].</td>
-         </tr>";
+$data="Helllo world";
 
 
 echo "data: {$data}\n\n";
