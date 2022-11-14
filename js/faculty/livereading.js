cbe88089b3827f7cae3c9468de2b327f1cf8a9b1
hr097 <harshilramani9777@gmail.com>
2022-11-14 13:39:45 +0530

    shu

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -13,7 +13,10 @@ function getLatestData()
     function (data, status) {
         if(status == "success")
         {           
-            $("#rfidcarddata").prepend(data);
+            if($("#rfidcarddata").html()!=data)
+            {
+                $("#rfidcarddata").prepend(data);
+            }
         }
     },"text"); // must write as text string will come
 }
@@ -24,45 +27,10 @@ $(document).ready(function(){
         setInterval(
         function ()
         {   
-
-    // let classroomid = $("#reader_selection").val();
-    // let csrfToken = $("#csrfToken").val();
-    // let cur_time = "2022-11-14 12:24:52";
-            
-    // $.post(
-    //     "api/amsapilength.php",
-    //     {
-    //         _cid: classroomid,
-    //         _ct: csrfToken,
-    //         _tm: cur_time
-    //     },
-    //     function (data, status) {
-    //         if(status == "success")
-    //         {   
-    //             let stud_len = $(".student").length;
-    //             console.log(stud_len); 
-
-    //             var old_len=0;
-
-    //             if(stud_len==0)
-    //             {
-                    getLatestData();
-        //         } 
-        //         else if(stud_len==data)
-        //         {
-
-        //         }
-        //     }
-        // },"text"); // must write as text string will come
-
+         getLatestData();
         },1000); 
 
     });
 
 });
 
-                    // //Card flip
-                    // var card = document.querySelector(".flip-card");
-                    // card.addEventListener("click", function () {
-                    //     card.classList.toggle("is-flipped");
-                    // });
