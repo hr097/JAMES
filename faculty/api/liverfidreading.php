7b6618eb5846d10ddb2a6d5b3cde8ca1aae2cee2
hr097 <harshilramani9777@gmail.com>
2022-11-14 13:37:00 +0530

    shu

--- faculty/api/liverfidreading.php
+++ faculty/api/liverfidreading.php
@@ -11,7 +11,7 @@ $JAMES->init_user_session();
 
 function getAmsApi($classroomid) 
 {   
-    $sql= "select Courses.course_name,Students.cur_semester,Students.cur_division,Students.spid,Students.name,Students.gender,DATE_FORMAT(Students.dob,'%d/%m/%Y') AS dob,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no(select MAX(Ams_api.reading_no) FROM Ams_api);";
+    $sql= "select Courses.course_name,Students.cur_semester,Students.cur_division,Students.spid,Students.name,Students.gender,DATE_FORMAT(Students.dob,'%d/%m/%Y') AS dob,Students.cur_semester FROM Students,Ams_api,Courses WHERE Ams_api.spid=Students.spid and Courses.course_id=Students.course_id and Ams_api.reader_no=$classroomid AND Ams_api.reading_no=(select MAX(Ams_api.reading_no) FROM Ams_api);";
 
     $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
     
