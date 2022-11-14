415e7393c71cc320da4390beb5faa9cb743613c8
hr097 <harshilramani9777@gmail.com>
2022-11-14 13:54:24 +0530

    shu

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -1,9 +1,14 @@
 
+var req_num =1;
+
 function getLatestData()
 {
     let classroomid = $("#reader_selection").val();
     let csrfToken = $("#csrfToken").val();
+    
+    if(req_num == 1)
     $("#rfidcarddata").html("");
+
     $.post(
     "api/liverfidreading.php",
     {
@@ -16,6 +21,7 @@ function getLatestData()
             if($("#rfidcarddata").html()!=data)
             {
                 $("#rfidcarddata").prepend(data);
+                req_num=2;
             }
         }
     },"text"); // must write as text string will come
