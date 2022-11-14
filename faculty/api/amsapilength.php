1a747044a751a92b992505a3dd9c9261af821cb8
hr097 <harshilramani9777@gmail.com>
2022-11-15 00:20:38 +0530

    oik

--- faculty/api/amsapilength.php
+++ faculty/api/amsapilength.php
@@ -2,6 +2,15 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-echo "data: The server time is: hello\n\n";
+$data="<tr class='student'>
+         <td>.record['spid'].</td>
+         <td>.record['name'].</td>
+        <td>.record['gender'].</td>
+        <td>.record['rdt'].</td>
+        <td>.record['cur_semester'].</td>
+         </tr>";
+
+
+echo "data: ".$data."\n\n";
 flush();
 ?>
