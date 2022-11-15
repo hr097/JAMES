eea152400ca9c0acb6feb4a928b8946c27d4af45
hr097 <harshilramani9777@gmail.com>
2022-11-15 15:10:29 +0530

    archit test

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -19,14 +19,14 @@ function getLatestData()
 
                 if(data==0)
                 {
-                    $("#rfidcarddata").empty();
-                    $("#rfidcarddata").html("<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>");
-                    console.log(data+"is no-data");
-                    data_bckp="<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>";
+                    // $("#rfidcarddata").empty();
+                   // $("#rfidcarddata").html("<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>");
+                    // console.log(data+"is no-data");
+                    // data_bckp="<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>";
                 }
                 else if(data!=data_bckp)
                 {   
-
+                    if(data_bckp!=data)
                     $("#rfidcarddata").prepend(data);
                     data_bckp=data;
                     console.log(data+"is there");
