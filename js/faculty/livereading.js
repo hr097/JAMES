91faa661015483ca1d8f77fdbd945b87e97e115b
hr097 <harshilramani9777@gmail.com>
2022-11-15 15:15:57 +0530

    archit test

--- js/faculty/livereading.js
+++ js/faculty/livereading.js
@@ -19,8 +19,8 @@ function getLatestData()
 
                 if(data==0)
                 {
-                    // $("#rfidcarddata").empty();
-                   // $("#rfidcarddata").html("<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>");
+                    $("#rfidcarddata").empty();
+                    // $("#rfidcarddata").html("<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>");
                     // console.log(data+"is no-data");
                     // data_bckp="<tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Latest Data Available</td></tr>";
                 }
@@ -29,7 +29,7 @@ function getLatestData()
                     if(data_bckp!=data)
                     $("#rfidcarddata").prepend(data);
                     data_bckp=data;
-                    console.log(data+"is there");
+                    
                 }
         }
     },"text"); // must write as text string will come
