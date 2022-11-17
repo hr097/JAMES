ee171ac19f14ef4072a2e120234fe6a16f6307b2
hr097 <harshilramani9777@gmail.com>
2022-11-17 23:25:16 +0530

    harshil: qr coded updated

--- temp/qrtest.php
+++ temp/qrtest.php
@@ -34,7 +34,7 @@ else
 
     
     
-    <img src="../assets/other/att_success2.gif" style="height:1000px;width:100%;margin-top:700px;" alt="Attendance marked">
+    <img src="../assets/other/att_success2.gif" style="height:1000px;width:100%;margin-top:300px;" alt="Attendance marked">
 
     </body>
 
