117b190766de003e5514b6a14241abfdb06087ac
hr097 <harshilramani9777@gmail.com>
2022-11-25 20:05:18 +0530

    harshil worked on student registration

--- admin/studentregistration.php
+++ admin/studentregistration.php
@@ -9,10 +9,180 @@ if(!($JAMES->checkSession()&&$_SESSION["_userType"]==="3"))
  $JAMES->ams_redirect("../login.php");
 }
 
+$error = "";
+$student = array("spid"=>"","name"=>"","gender"=>"","dob"=>"","email"=>"","contact_no"=>"","course_name"=>"","joining_year"=>"","cur_semester"=>"","cur_division"=>"","cur_roll_no"=>"","stud_status"=>"","fathers_name"=>"","fathers_email"=>"","fathers_contact"=>"","mothers_name"=>"","mothers_email"=>"","mothers_contact"=>"");
+
+$button = "";
 
 if(isset($_GET["spid"]))
+{   
+    $spid = $_GET["spid"];
+
+    $sql= "select A.*,B.* from Students A,Courses B where A.course_id=B.course_id AND A.spid='$spid';";
+
+    $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
+    
+    if(mysqli_num_rows($result)==1)
+    {
+        $student = mysqli_fetch_assoc($result);
+        $button = "<button type='submit' id='updtstud' name='updtstud' class='btn btn-primary mr-2 mt-3'>Update Student</button>";
+    }
+    else
+    {
+       $error="<span style='color:red;float:right;'>SPID Not Found!</span>";
+       $error.="<script>setTimeout(function(){window.location.href='studentregistration.php';},3000);</script>";
+    }
+
+}
+else
 {
- echo "<script>alert('stud');</script>";
+    $button = " <button type='submit' id='addstud' name='addstud' class='btn btn-primary mr-2 mt-3'>Add Student</button>";
+}
+
+
+$genderBox= "";
+$statusBox = "";
+$courseBox = "";
+$div_array = array("A","B","C","D","E","F","G","H","I");
+$div_html = "<select name='div_Selection' class='form-control' required><option>Not Selected</option>";
+
+foreach($div_array as $key => $value)
+{       
+    $s3 = "";
+    if($student['cur_division']==$value)
+    {
+        $s3="selected";
+    }
+    $div_html .= "<option value='".$value."' ".$s3.">".$value."</option>";
+}
+
+$div_html .= "</select>";
+
+//courses fetch for dropdown
+$sql= "select * from Courses;";//query
+$result = mysqli_query($JAMES->connection(),$sql);
+
+if(mysqli_num_rows($result)>0)
+{
+    $course_html = "<select name='course_Selection' id='course_selection' class='form-control' required><option value=''>Not Selected</option></option>";
+    $sem_html = "<select  name='sem_Selection'class='form-control' required><option>Not Selected</option>";
+
+
+    //course
+    while($record = mysqli_fetch_assoc($result))
+    { 
+      $select="";
+      if($student['course_name']==$record['course_name'])
+      {
+        $select="selected";
+
+        //semester
+        $ts = $student['total_semester'];
+
+        $i = 1;
+
+
+        while($i<=$ts)
+        {    
+            $s2="";
+            if($student['cur_semester']==$i)
+            {
+                $s2="selected";
+
+            }
+            $sem_html.="<option value='".$i."' ".$s2.">".$i."</option>";
+            $i++;
+        }
+
+        $sem_html.="</select>";
+
+      }
+
+      $course_html.="<option value='".$record['course_name']."' ".$select.">".$record['course_name']."</option>";
+    }
+
+    $course_html.="</select>";
+}
+else
+{
+  $course_html = 
+  "
+  <select name='course_Selection' class='form-control' required>
+    <option value=''>Not Selected</option>
+  </select>
+  ";
+}
+
+
+
+if($student['stud_status']==0)
+{
+
+    $statusBox = "
+    <label>Student Status</label>
+    <select name='status' class='form-control'required>
+        <option value='1'>Active</option>
+        <option value='0' selected>InActive</option>
+    </select>
+    </div>
+    ";
+
+}
+else
+{
+    $statusBox = "
+    <label>Student Status</label>
+    <select name='status' class='form-control' required>
+            <option value='1' selected>Active</option>
+            <option value='0'>InActive</option>
+    </select>
+    </div>
+    ";
+}
+
+if($student['gender']=='Male')
+{
+
+    $genderBox= "
+    <div class='form-group col-sm-6 col-md-6 col-lg-6'>
+        <label>Gender</label>
+        <select name='status' class='form-control' required>
+            <option value=''>Not Selected</option>
+            <option value='Male' selected>Male</option>
+            <option value='Female'>Female</option>
+        </select>
+    </div>
+    ";
+
+
+}
+else if($student['gender']=='Female')
+{
+    $genderBox= "
+    <div class='form-group col-sm-6 col-md-6 col-lg-6'>
+        <label>Gender</label>
+        <select name='status' class='form-control' required>
+            <option value=''>Not Selected</option>
+            <option value='Male'>Male</option>
+            <option value='Female'selected>Female</option>
+        </select>
+    </div>
+    ";
+}
+else
+{
+
+    $genderBox= "
+        <div class='form-group col-sm-6 col-md-6 col-lg-6'>
+            <label>Gender</label>
+            <select name='status' class='form-control' required>
+                <option value=''>Not Selected</option>
+                <option value='Male'>Male</option>
+                <option value='Female'>Female</option>
+            </select>
+        </div>
+        ";
+
 }
 
 ?>
@@ -51,8 +221,8 @@ if(isset($_GET["spid"]))
                 <div class="col-sm-12  col-md-12  col-lg-12  grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Student Regisration</h4>
-                            <form class="forms-sample" >
+                            <h4 class="card-title">Student Regisration <?php echo $error;?></h4>
+                            <form class="forms-sample" name="addstudents" action='studentregistration.php' method="POST" enctype="multipart/form-data">
 
                                 <input type="hidden" id="csrfToken" name="_csrfToken" value="<?php echo $JAMES->generateCsrfToken();?>" >
 
@@ -60,36 +230,29 @@ if(isset($_GET["spid"]))
                                 <div class="row">
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>SPID</label>
-                                        <input type="text" class="form-control" placeholder="XXXXXXXXXX" required>
+                                        <input type="text" class="form-control" id="studspid" placeholder="XXXXXXXXXX" value="<?php echo $student['spid'];?>" required>
                                     </div>
 
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Email</label>
-                                        <input type="email" class="form-control" placeholder="example@vnsgu.ac.in">
+                                        <input type="email" class="form-control" id="studemail" placeholder="example@vnsgu.ac.in" value="<?php echo $student['email'];?>" required>
                                     </div>
                                 </div>
 
                                 <!-- Name-->
                                 <div class="form-group">
                                     <label>Name</label>
-                                    <input type="text" class="form-control" placeholder="Enter Student Name">
+                                    <input type="text" class="form-control" id="studname" placeholder="Enter Student Name" value="<?php echo $student['name'];?>" required>
                                 </div>
 
 
                                 <!-- Gender and DOB -->
                                 <div class="row">
-                                    <div class="form-group col-sm-6 col-md-6 col-lg-6">
-                                        <label>Gender</label>
-                                        <select class="form-control">
-                                            <option>Not Selected</option>
-                                            <option>Male</option>
-                                            <option>Female</option>
-                                        </select>
-                                    </div>
+                                    <?php echo $genderBox;?>
 
                                     <div class="form-group col-md-6">
-                                        <label>DOB</label>
-                                        <input type="date" class="form-control" id="" value="">
+                                        <label>Birthdate</label>
+                                        <input type="date" class="form-control" id="studdob" value="<?php echo $student['dob'];?>" required>
                                     </div>
                                 </div>
 
@@ -97,24 +260,19 @@ if(isset($_GET["spid"]))
                                 <div class="row">
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Contact No</label>
-                                        <input type="number" class="form-control" id="" value=""
-                                            placeholder="+91 XXXXXXXXXX">
+                                        <input type="text" class="form-control" id="studcontact" value="<?php echo $student['contact_no'];?>" placeholder="+91 XXXXXXXXXX" required>
+                                            
                                     </div>
 
                                     <div class="form-group col-md-6">
                                         <label>Joining Year</label>
-                                        <input type="date" class="form-control" id="" value="">
+                                        <input type="text" class="form-control" id="studjoinyear" value="<?php echo $student['joining_year'];?>" required>
                                     </div>
                                 </div>
 
                                 <!-- Status-->
                                 <div class="form-group mb-5 ">
-                                <label>Status</label>
-                                    <select class="form-control">
-                                            <option>Active</option>
-                                            <option>InActive</option>
-                                    </select>
-                                </div>
+                                <?php echo $statusBox; ?>
 
 
                                 <!-- Course Details -->
@@ -123,19 +281,12 @@ if(isset($_GET["spid"]))
                                 <div class="row">
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Course Name</label>
-                                        <select class="form-control">
-                                            <option>Not Selected</option>
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                        </select>
+                                        <?php echo $course_html;?>
                                     </div>
 
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Current Roll No</label>
-                                        <input type="number" class="form-control" id="" value=""
-                                            placeholder="Enter Roll No">
+                                        <input type="number" name="studrollno" class="form-control" id="studrollno" value="<?php echo $student['cur_roll_no'];?>" placeholder="Enter Roll No" required> 
                                     </div>
                                 </div>
 
@@ -143,24 +294,12 @@ if(isset($_GET["spid"]))
                                 <div class="row">
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Current Semester </label>
-                                        <select class="form-control">
-                                            <option>Not Selected</option>
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                        </select>
+                                        <?php echo $sem_html; ?>
                                     </div>
 
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Current Division</label>
-                                        <select class="form-control">
-                                            <option>Not Selected</option>
-                                            <option>A</option>
-                                            <option>B</option>
-                                            <option>c</option>
-                                            <option>D</option>
-                                        </select>
+                                        <?php echo $div_html; ?>
                                     </div>
                                 </div>
 
@@ -170,49 +309,46 @@ if(isset($_GET["spid"]))
 
                                 <div class="form-group">
                                     <label>Father's Name</label>
-                                    <input type="text" class="form-control" placeholder="Enter Father's Name">
+                                    <input type="text" name="fname" class="form-control"  value="<?php echo $student['fathers_name'];?>" placeholder="Enter Father's Name">
                                 </div>
 
 
                                 <div class="row">
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Father's Email</label>
-                                        <input type="text" class="form-control" id="" value=""
-                                            placeholder="example@gmail.com">
+                                        <input type="text" name="femail" class="form-control" id="femail"  value="<?php echo $student['fathers_email'];?>"  placeholder="example@gmail.com">
+                                           
                                     </div>
 
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Father's Contact</label>
-                                        <input type="number" class="form-control" id="" value=""
-                                            placeholder="+91 XXXXXXXXXX">`
+                                        <input type="text" name="fcontact" class="form-control" id="fcontact"  value="<?php echo $student['fathers_contact'];?>" placeholder="+91 XXXXXXXXXX">
+                                            
                                     </div>
                                 </div>
 
                                 <div class="form-group">
                                     <label>Mother's Name</label>
-                                    <input type="text" class="form-control" placeholder="Enter Mother's Name">
+                                    <input type="text" name="mname" class="form-control" id="mname" placeholder="Enter Mother's Name"  value="<?php echo $student['mothers_name'];?>">
                                 </div>
 
 
                                 <div class="row">
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Mother's Email</label>
-                                        <input type="text" class="form-control" id="" value=""
-                                            placeholder="example@vnsgu.ac.in">
+                                        <input type="text" name="memail" class="form-control" id="memail"  value="<?php echo $student['mothers_email'];?>" placeholder="example@vnsgu.ac.in">
+                                            
                                     </div>
 
                                     <div class="form-group col-sm-6 col-md-6 col-lg-6">
                                         <label>Mother's Contact</label>
-                                        <input type="number" class="form-control" id="" value=""
-                                            placeholder="+91 XXXXXXXXXX">
+                                        <input type="text" class="form-control" id="mcontact"  value="<?php echo $student['mothers_contact'];?>" placeholder="+91 XXXXXXXXXX">
+                                            
                                     </div>
-
-
-
 
                                 </div>
 
-                                <button type="submit" id="" class="btn btn-primary mr-2 mt-3">Add Student</button>
+                               <?php echo $button; ?>
                                 <button class="btn btn-light mt-3">Clear</button>
                             </form>
                         </div>
