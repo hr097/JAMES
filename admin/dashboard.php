6ae28484739360ee30f3fd59b0df64ddbb7fdf0f
hr097 <harshilramani9777@gmail.com>
2022-12-01 21:48:13 +0530

    remodified

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,61 +1,71 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin:ams.vnsguit.org'); 
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type, Access-Control-Allow-Methods,Authorization');
 
- require_once("../ams.php");
- $JAMES = new AMS("Admin");
- $JAMES->init_user_session();
+require_once("../../ams.php");
+$JAMES = new AMS("User");
+$JAMES->init_user_session();
 
+function getClassrooms($year) 
+{ 
+    $sql= "select A.*,C.course_name,S.subject_code,F.email from Ams_setup_course_subject_map A,
+    Courses C,
+    Subjects S,
+    Course_subject_map CSM,
+    Ams_setup_faculties_map ASFM,
+    Faculties F 
+    where
+    A.ams_setup_id = ASFM.ams_setup_id and
+    ASFM.fid = F.fid and
+    A.cs_id=CSM.cs_id and
+    C.course_id = CSM.course_id and
+    S.subject_id = CSM.subject_id and
+    A.year=$year;";
 
- if(!($JAMES->checkSession()&&$_SESSION["_userType"]==="3"))
- {
-  $JAMES->ams_redirect("../login.php");
- }
- 
- ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-
-        <!-- including header -->
-        <?php
-        include './common/header.php'
-        ?>
-
-        <!-- css  -->
-        <link rel="stylesheet" href="../css/admin.css">
-        <link rel="stylesheet" href="../css/student.css">
-        
-        <!-- js  -->
-        <script src="../js/admin/dashboard.js" type="text/javascript" defer=true></script>
-        
-        
-
-        <!-- page information-->
-        <title>AMS | Admin Dashboard</title>
-        
-    </head>
-
-    <body>
-
-                <!-------------------------------------------------------Main Content------------------------------------------------------->
-                <div class="main-panel">
-                    <div class="content-wrapper">
-                        <div class="row" >
-                            <div class="col-12 col-xl-8 mb-4 mb-xl-50">
-                                <h3 class="font-weight-bold greet">Welcome AMS Admin,</h3>
-                                <h6 id="daymode" class="font-weight-normal mb-10"></h6>
-                            </div>
-                    </div>
-                </div>
-
-        <!-- including footer -->
-        <?php
-        include './common/footer.php'
-        ?>
-
-    </body>
+    $result = mysqli_query($GLOBALS['JAMES']->connection(),$sql);
+    
+    if(mysqli_num_rows($result)>=1)
+    {
+        $ams_setup = "";
+        while($record = mysqli_fetch_assoc($result))
+        {  
 
 
-</html>
+            $ams_setup.=
+            "
+            <tr class='ams_setup'>
+            <td>".$record['ams_setup_id']."</td>
+            <td>".$record['course_name']."</td>
+            <td>".$record['subject_code']."</td>
+            <td>".$record['year']."</td>
+            <td>".$record['email']."</td>
+            </tr>
+            ";
+        }
+        return $ams_setup;
+    }
+    else
+    {
+        return "
+        <tr><td  colspan='5' style='font-size:1.2em;text-align:center;'>No Classrooms Data Found!</td></tr>";
+    }
+}
 
+
+
+
+if(isset($_POST['_yr'])&&isset($_POST['_ct'])&&$_POST['_ct']==$_SESSION['_csrfToken']&&isset($_SESSION['_userId']))
+{
+        $year = $JAMES->sanitizeInput($_POST['_yr']);
+        echo(getClassrooms($year));
+}
+else
+{    
+    $JAMES->ams_redirect("../../login.php"); // when outside request comes redirect to login
+}
+
+
+
+?>
