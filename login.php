254f4040be977217be9e80b62e1779cc141f3334
hr097 <harshilramani9777@gmail.com>
2022-12-09 21:09:04 +0530

    test pwa

--- login.php
+++ login.php
@@ -59,6 +59,7 @@ if($JAMES->checkSession()===true) // if session active than redirect user to his
 
     <!-- pwa application -->
     <link rel="manifest" href="manifest.json">
+    <script src="pwa.js" type="text/javascript" defer=true></script>
 
 </head>
 
