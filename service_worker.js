5ca78c24a4f872eb7249eb5504d9f83d05392eff
hr097 <harshilramani9777@gmail.com>
2022-12-09 21:11:16 +0530

    test pwa

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
       caches.open("static").then(cache=>{
-        return cache.addAll(["./",'./images/logo192.png']);
+        return cache.addAll(["./",'assets/pwa-logos/192_logo.png']);
       })
     );
   });
