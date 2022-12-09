6990f1f751952f75d3da5293c5b79f6c2212ad87
hr097 <harshilramani9777@gmail.com>
2022-12-09 21:18:54 +0530

    test pwa

--- ams.php
+++ ams.php
@@ -87,8 +87,8 @@ class AMS
                         }
                 default: 
                     {
-                        $this->userName = "shubham"; // localhost credentials
-                        $this->password = "shubham";
+                        $this->userName = "root"; // localhost credentials
+                        $this->password = "";
                         break; 
                     }
             }
